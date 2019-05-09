659b03d0c7c1abbdef069d5148354787fdfe1135
Stefanie K <stefkenyon1126@gmail.com>
2019-05-09 19:14:11 -0400

    Complete queries

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # Analyzing Data With PostgreSQL
+
+Given several CSV files relating to:
+* Employees
+* Departments
+* Salaries
+* Job Titles
+* Department Managers
+* Department Employees
+
+## Data Modeling & Company Entity Relationship Diagram
+After evaluating each CSV for data and column headings, the Entity Relationship Diagram was created to show how the data in the CSVs relate to one another. Here we looked at entities, the primary and foreign keys, and the attributes that belong to those entities. 
+
+![Company_ERD](Images/Company_ERD.png)
+
+## Data Engineering
+After creating the ERD, we created the database schema including the tables with the required primary and foreign keys and other attributes that belong to the tables. Then the data in the CSVs were imported into their respective tables.
+
+## Data Analysis
+Using the information in the tables, queries were created to obtain the requested information from the tables.
+
---
+++ src/queries.sql
@@ -0,0 +1,41 @@
+--Query 1
+SELECT a.emp_no, a.first_name, a.last_name, a.gender, b.salary
+FROM employees a, salaries b
+WHERE a.emp_no = b.emp_no;
+
+--Query 2
+SELECT a.emp_no, a.first_name, a.last_name
+FROM employees a
+WHERE EXTRACT(year FROM hire_date) = 1986;
+
+--Query 3
+SELECT a.dept_no, a.dept_name, b.emp_no, c.last_name, c.first_name, b.from_date, b.to_date
+FROM departments a, dept_manager b, employees c
+WHERE a.dept_no = b.dept_no AND b.emp_no = c.emp_no;
+
+--Query 4
+SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees a, dept_emp b, departments c
+WHERE a.emp_no = b.emp_no AND b.dept_no = c.dept_no;
+
+--Query 5
+SELECT *
+FROM employees a
+WHERE a.first_name = 'Hercules' AND a.last_name LIKE 'B%';
+
+--Query 6
+SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees a, dept_emp b, departments c
+WHERE a.emp_no = b.emp_no AND b.dept_no = c.dept_no AND c.dept_name = 'Sales';
+
+--Query 7
+SELECT a.emp_no, a.last_name, a.first_name, c.dept_name
+FROM employees a, dept_emp b, departments c
+WHERE a.emp_no = b.emp_no AND b.dept_no = c.dept_no AND (c.dept_name = 'Sales' OR 
+	c.dept_name = 'Development');
+	
+--Query 8
+SELECT a.last_name, COUNT(last_name)
+FROM employees a
+GROUP BY a.last_name
+ORDER BY COUNT(last_name) DESC;
